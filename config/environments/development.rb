f945c24538d343c8530213083cfa3974702e191e
scosy <amine.bentaleb064@gmail.com>
2022-04-12 07:06:44 +0200

    Chapter 5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.cache_store = :redis_cache_store, { url: "redis://<my_server>:6379/0" }
   config.cache_classes = false
 
   # Do not eager load code on boot.
